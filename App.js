848ef4ad3fc7656a32bd8cb504faef60b520432b
RD-Carl <carl@local.tc>
2024-03-29 17:04:41 +0800

    feat: init

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { config } from "@tamagui/config/v3";
+import "@tamagui/core/reset.css";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TamaguiProvider, createTamagui } from "tamagui";
+const tamaguiConfig = createTamagui(config);
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={tamaguiConfig}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <Text style={{ fontFamily: "InterBold", fontSize: 30 }}>
+          Open up App.js to start working on your app!
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </TamaguiProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
